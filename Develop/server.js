82f935aa684734340a4352f976791c83063ebc63
JillWesterfelhaus <jillwesthaus@gmail.com>
2020-06-02 14:25:23 -0500

    Update server.js

--- Develop/server.js
+++ Develop/server.js
@@ -12,6 +12,62 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populate", { useNewUrlParser: true });
 
-app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}!`);
+//from activity 14
+
+
+db.Workout.create({ name: "Campus Workout" })
+  .then(dbWorkout => {
+    console.log(dbWorkout);
+  })
+  .catch(({message}) => {
+    console.log(message);
   });
+
+app.post("/submit", ({body}, res) => {
+  db.Exercise.create(body)
+    .then(({_id}) => db.Workout.findOneAndUpdate({}, { $push: { Exercises: _id } }, { new: true }))
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+app.get("/exercises", (req, res) => {
+  db.Book.find({})
+    .then(dbExercise => {
+      res.json(dbExercise);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+app.get("/workout", (req, res) => {
+  db.Workout.find({})
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+app.get("/populated", (req, res) => {
+  db.Workout.find({})
+    .populate("exercises")
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
+
+
+
