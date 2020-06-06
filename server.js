0b58e4a647e9328e6832b8d770866cc626c26d9f
JillWesterfelhaus <jillwesthaus@gmail.com>
2020-06-06 12:40:02 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3000;
+
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populate", { useNewUrlParser: true });
-//async/await; define db elsewhere
+//mongoose.connect in seed.js
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populate", 
+{ useNewUrlParser: true });
+
+//async/await? define db elsewhere?
+//why need to define db if created here?
 db.Workout.create({ name: "Workout" })
   .then(dbWorkout => {
     console.log(dbWorkout);
