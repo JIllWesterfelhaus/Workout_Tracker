8cf04a50812646ba7e64c138e248b67515e20b61
JillWesterfelhaus <jillwesthaus@gmail.com>
2020-07-12 13:58:12 -0500

    Updated seed.js. replaced public folder with public folder from Develop

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,7 +1,7 @@
 let mongoose = require("mongoose");
 let db = require("../models");
 
-mongoose.connect( process.env.MONGODB_URL || "mongodb://localhost/workoutv3", {
+mongoose.connect( process.env.MONGODB_URL || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
